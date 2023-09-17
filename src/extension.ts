c8cac73dbd2ede06270c906eaa5287c084b412ba
Martin Svanberg <martin@martinsvanberg.com>
2023-09-17 18:02:42 +0200

    Better formatting for uncommitted changes

--- src/extension.ts
+++ src/extension.ts
@@ -95,12 +95,7 @@ export function activate(context: vscode.ExtensionContext) {
           0
         );
 
-        const elapsed = relativeTimePassed(
-          Date.now(),
-          parseInt(fields["author-time"]) * 1000
-        );
-
-        const message = `${fields.author}, ${elapsed} • ${fields.summary}`;
+        const message = formatMessage(fields);
 
         const hoverMessage = Object.entries(fields)
           .map((entry) => `- **${entry[0]}**: \`${entry[1]}\``)
@@ -131,4 +126,17 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-export function deactivate() {}
+function formatMessage(fields: Record<string, string>): string {
+  const isUncommitted = fields["author"] === "Not Committed Yet";
+  if (isUncommitted) {
+    return 'Not committed yet';
+  }
+  const elapsed = relativeTimePassed(
+    Date.now(),
+    parseInt(fields["author-time"]) * 1000
+  );
+
+  return `${fields.author}, ${elapsed} • ${fields.summary}`;
+}
+
+export function deactivate() { }
